8e158909962e1c1275bdc0ff8c5b0fdca4ec93ec
thabaptiser <baptiste.vauthey@gmail.com>
2016-11-07 02:22:52 +0000

    switching to trees

--- scraper.py
+++ scraper.py
@@ -7,12 +7,12 @@ import time
 wiki = "https://en.wikipedia.org/wiki/{page}"
 
 def scrape_page(url):
-  print("starting: " + url)
-  try:
-    Page.nodes.get(link=unicode(url, "utf-8"))
-    return
-  except Page.DoesNotExist:
-    pass
+  #print("starting: " + url)
+  #try:
+  #  Page.nodes.get(link=unicode(url, "utf-8"))
+  #  return
+  #except Page.DoesNotExist:
+  #  pass
   try:
     page = urllib2.urlopen(url)
   except:
@@ -31,6 +31,7 @@ def scrape_page(url):
   try:
     new_page = Page.nodes.get(link=unicode(url, "utf-8"))
     if new_page.page_text == data:
+        print("already parsed: " + url)
     	return
     else:
         new_page.delete()
@@ -51,28 +52,64 @@ def scrape_page(url):
   print("finished: " + url)
 
 
-print("opening file")
-with open('./enwiki-latest-all-titles-in-ns0') as f:
-  print("opened file")
-  x = 0
-  t = []
-  for page in f:
-    count = 0
-    for c in page:
-      if c.isdigit():
-        count += 1
-    if count > 0:
-       continue
-    x+=1
-    if x%10000==0:
-      t.append(Thread(target=scrape_page, args=[wiki.format(page=page),]))
-      t[-1].start()
-    if len(t) > 10:
-       done = False
-       while not done:
-         time.sleep(0.1)
-         for i in t:
-           if not i.isAlive():
-             done=True
-             t.remove(i)
-             break
+#print("opening file")
+#with open('./enwiki-latest-all-titles-in-ns0') as f:
+#  print("opened file")
+#  x = 0
+#  t = []
+#  for page in f:
+#    count = 0
+#    for c in page:
+#      if c.isdigit():
+#        count += 1
+#    if count > 0:
+#       continue
+#    x+=1
+#    if x<500000:
+#        continue
+#    if x%1000==1:
+#      t.append(Thread(target=scrape_page, args=[wiki.format(page=page),]))
+#      t[-1].start()
+#    if len(t) > 50:
+#       done = False
+#       while not done:
+#         time.sleep(0.1)
+#         for i in t:
+#           if not i.isAlive():
+#             done=True
+#             t.remove(i)
+#             break
+#orig_page = urllib2.urlopen("https://en.wikipedia.org/wiki/Category:Trees")
+#result = soup.find("div", {"class":"mw-content-ltr"}).find_all('a', href=True)
+#list_links = ['https://en.wikipedia.org/' + a['href'] for a in result]
+#orig_page = urllib2.urlopen("https://en.wikipedia.org/wiki/Category:Trees")
+
+prev_seen = []
+
+def search(url):
+  if url in prev_seen:
+    return []
+  prev_seen.append(url)
+  ret =set([])
+  page = urllib2.urlopen(url)
+  soup = BeautifulSoup(page, "html.parser")
+  result = soup.find("div", {"class":"mw-category"})
+  result_sub = soup.find("div", {"class":"CategoryTreeItem"}) 
+  result_pages = soup.find("div", {"id":"mw-pages"}) 
+  list_links = []
+  if result: list_links += [a['href'] for a in result.find_all('a', href=True)]
+  if result_sub: list_links += [a['href'] for a in result_sub.find_all('a', href=True)]
+  if result_pages: list_links += [a['href'] for a in result_pages.find_all('a', href=True)]
+  if not list_links:
+    print(url)
+  for l in list_links:
+    if l[0:6] == "/wiki/":
+      if "/wiki/Category:" in l:
+        ret = ret.union(search("https://en.wikipedia.org" + l))
+      else:
+        print("adding: " + l)
+        ret.add(l)
+  return ret
+
+for l in search("https://en.wikipedia.org/wiki/Category:Trees"):
+  scrape_page(l)
